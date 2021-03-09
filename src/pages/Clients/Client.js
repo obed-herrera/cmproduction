eff9ac9c6a4f394aa231d912d3c6d02bd8add10a
Obed Herrera <skrillexislove@gmail.com>
2021-03-08 21:33:04 -0600

    Trabajo dentro de metodo insertado de clientes

--- src/pages/Clients/Client.js
+++ src/pages/Clients/Client.js
@@ -1,18 +1,16 @@
 import {React, useState, useEffect} from 'react'
-import ClientForm from "./ClientForm";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageHeader from "../../components/PageHeader";
 import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleAltOutlined'
-import {Paper, makeStyles, TableBody, TableRow, TableCell} from '@material-ui/core';
+import {Paper, makeStyles} from '@material-ui/core';
 import useTable from "../../components/useTable";
 import * as clientServices from "../../services/clientServices";
 import Copyright from '../../components/Copyright';
 import axios from 'axios';
 import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
-import { Grid, TextField, Typography} from '@material-ui/core';
+import  {Grid}  from '@material-ui/core';
 import "./ClientStyles.css";
-import {useForm, Form} from '../../components/useForm';
-import Controls from "../../controls/Controls";
+
 const useStyle = makeStyles(theme => ({
     pageContent:{
         margin: theme.spacing(5),
@@ -41,8 +39,7 @@ const Client = (props) =>{
         client_home_address: '',
         client_business_address: '',
         client_state: '',
-        client_line: ''
-        
+        client_line: ''    
     });
 
     const handleChange=e=>{
@@ -68,6 +65,28 @@ const Client = (props) =>{
     }
 
     const peticionesPost=async()=>{
+        var f = new FormData();
+        f.append("client_first_name", clienteSeleccionado.client_first_name);
+        f.append("client_second_name", clienteSeleccionado.client_second_name);
+        f.append("client_middle_name", clienteSeleccionado.client_middle_name);
+        f.append("client_last_name", clienteSeleccionado.client_last_name);
+        f.append("client_national_id", clienteSeleccionado.client_national_id);
+        f.append("client_sys_code", clienteSeleccionado.client_sys_code);
+        f.append("client_home_address", clienteSeleccionado.client_home_address);
+        f.append("client_business_address", clienteSeleccionado.client_business_address);
+        f.append("client_state", clienteSeleccionado.client_state);
+        f.append("client_line", clienteSeleccionado.client_line);
+        f.append("METHOD", "POST");
+        await axios.post(baseUrl, f)
+        .then(response=>{
+        setData(data.concat(response.data));
+        abrirCerrarModalInsertar();
+        }).catch(error=>{
+        console.log(error);
+        })
+    }
+
+    const peticionPost=async()=>{
         var f = new FormData();
         f.append("client_first_name", clienteSeleccionado.client_first_name);
         f.append("client_second_name", clienteSeleccionado.client_second_name);
@@ -121,22 +140,32 @@ const Client = (props) =>{
         <tr>
           <th>ID</th>
           <th>Primer Nombre</th>
+          <th>Segundo Nombre</th>
           <th>Primer Apellido</th>
+          <th>Segundo Apellido</th>
           <th>Cedula</th>
+          <th>Codigo del Cliente</th>
+          <th>Direccion de casa</th>
+          <th>Direccion de negocio</th>
+          <th>Estado del Cliente</th>
           <th>Linea</th>
-          <th>Direccion</th>
           <th>Acciones</th>
         </tr>
         </thead>
         <tbody>
-            {data.map(client=>(
-                <tr key={client.id_credi_client}>
-                    <td>{client.id_credi_client}</td>
-                    <td>{client.client_first_name}</td>
-                    <td>{client.client_middle_name}</td>
-                    <td>{client.client_national_id}</td>
-                    <td>{client.client_line}</td>
-                    <td>{client.client_home_address}</td>
+            {data.map(Client=>(
+                <tr key={Client.id_credi_client}>
+                    <td>{Client.id_credi_client}</td>
+                    <td>{Client.client_first_name}</td>
+                    <td>{Client.client_second_name}</td>
+                    <td>{Client.client_middle_name}</td>
+                    <td>{Client.client_last_name}</td>
+                    <td>{Client.client_national_id}</td>
+                    <td>{Client.client_sys_code}</td>
+                    <td>{Client.client_home_address}</td>
+                    <td>{Client.client_business_address}</td>
+                    <td>{Client.client_state}</td>
+                    <td>{Client.client_line}</td>
                     <td>
                         <button className = "btn btn-primary">Editar</button>{"   "}
                         <button className = "btn btn-danger">Eliminar</button>
@@ -146,69 +175,66 @@ const Client = (props) =>{
         </tbody> 
 
     </table>
-        <div class="modal-dialog modal-lg">
         <Modal isOpen={modalInsertar} contentClassName = "custom-modal-style">
-      <ModalHeader>Insertar Framework</ModalHeader>
-      <ModalBody>
-        {/*<ClientForm/>*/}
-        <div className="form-group">
-            <Grid container spacing = {2}>
-            <Grid item xs ={4}>
-                <label>Primer Nombre: </label>
-                <br />
-                <input type="text" className="form-control" name="client_first_name" onChange={handleChange}/>
-                <br />
-                <label>Segundo Nombre: </label>
-                <br />
-                <input type="text" className="form-control" name="client_second_name" onChange={handleChange}/>
-                <br />
-                <label>Primer Apellido: </label>
-                <br />
-                <input type="text" className="form-control" name="client_middle_name" onChange={handleChange}/>
-                <br />
-                <label>Segundo Apellido: </label>
-                <br />
-                <input type="text" className="form-control" name="client_last_name" onChange={handleChange}/>
-                <br />
-            </Grid>
-            <Grid item xs ={4}>
-                <label>Cedula: </label>
-                <br />
-                <input type="text" className="form-control" name="client_national_id" onChange={handleChange}/>
-                <br />
-                <label>Codigo del Sistema: </label>
-                <br />
-                <input type="text" className="form-control" name="client_sys_code" onChange={handleChange}/>
-                <br />
-                <label>Direccion de Casa: </label>
-                <br />
-                <input type="text" className="form-control" name="client_home_address" onChange={handleChange}/>
-                <br />
-                <label>Direccion del Negocio: </label>
-                <br />
-                <input type="text" className="form-control" name="client_business_address" onChange={handleChange}/>
-                <br />
-            </Grid>
-            <Grid item xs ={4}>
-                <label>Linea: </label>
-                <br />
-                <input type="text" className="form-control" name="client_line" onChange={handleChange}/>
-                <br />
-                <label>Estado del Cliente: </label>
-                <br />
-                <input type="text" className="form-control" name="client_state" onChange={handleChange}/>
-                <br />
-                    
-            </Grid>
-            </Grid>
-        </div> 
-      </ModalBody>
-      <ModalFooter>
-            <button className="btn btn-primary" onClick={()=>peticionesPost()}>Insertar</button>{"   "}
+            <ModalHeader>Insertar Cliente</ModalHeader>
+            <ModalBody>
+                {/*<ClientForm/>*/}
+                <div className="form-group">
+                    <Grid container spacing = {2}>
+                        <Grid item xs ={4}>
+                            <label>Primer Nombre: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_first_name" onChange={handleChange}/>
+                            <br />
+                            <label>Segundo Nombre: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_second_name" onChange={handleChange}/>
+                            <br />
+                            <label>Primer Apellido: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_middle_name" onChange={handleChange}/>
+                            <br />
+                            <label>Segundo Apellido: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_last_name" onChange={handleChange}/>
+                            <br />
+                        </Grid>
+                        <Grid item xs ={4}>
+                            <label>Cedula: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_national_id" onChange={handleChange}/>
+                            <br />
+                            <label>Codigo del Sistema: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_sys_code" onChange={handleChange}/>
+                            <br />
+                            <label>Direccion de Casa: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_home_address" onChange={handleChange}/>
+                            <br />
+                            <label>Direccion del Negocio: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_business_address" onChange={handleChange}/>
+                            <br />
+                        </Grid>
+                        <Grid item xs ={4}>
+                            <label>Estado del Cliente: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_state" onChange={handleChange}/>
+                            <br />  
+                            <label>Linea: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_line" onChange={handleChange}/>
+                            <br /> 
+                        </Grid>
+                    </Grid>
+                </div> 
+            </ModalBody>
+            <ModalFooter>
+            <button className="btn btn-primary" onClick={()=>peticionPost()}>Insertar</button>{"   "}
             <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button>
       </ModalFooter>
     </Modal>
-            </div>
         </div>
     </Paper>
         
--- src/pages/Employees/Employee.js
+++ src/pages/Employees/Employee.js
@@ -7,6 +7,9 @@ import {Paper, makeStyles, TableBody, TableRow, TableCell} from '@material-ui/co
 import useTable from "../../components/useTable";
 import * as employeeServices from "../../services/employeeServices";
 import axios from 'axios';
+import "./EmployeeStyles.css";
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import  {Grid}  from '@material-ui/core';
 
 const useStyle = makeStyles(theme => ({
     pageContent:{
@@ -26,6 +29,33 @@ export default function Employee(){
 
     const baseUrl = "http://localhost/crediapi/employee.php";
     const [data, setData] = useState([]);
+    const [modalInsertar, setModalInsertar] = useState(false);
+    const [empleadoSeleccionado, setEmpleadoSeleccionado]=useState({
+        id_employee: '',
+        employee_first_name: '',
+        employee_second_name: '',
+        employee_middle_name: '',
+        employee_last_name:'',
+        employee_email: '',
+        employee_phone: '',
+        employee_address: '',
+        employee_creation_date: '',
+        employee_type: '',
+        employee_state: ''    
+    });
+
+    const handleChange=e=>{
+        const {name, value}=e.target;
+        setEmpleadoSeleccionado((prevState)=>({
+          ...prevState,
+          [name]: value
+        }))
+        console.log(empleadoSeleccionado);
+      }
+
+    const abrirCerrarModalInsertar = () =>{
+        setModalInsertar(!modalInsertar);
+    }
 
     const peticionesGet = async()=>{
         await axios.get(baseUrl)
@@ -36,17 +66,34 @@ export default function Employee(){
         })
     }
 
+    const peticionesPost=async()=>{
+        var f = new FormData();
+        f.append("employee_first_name", empleadoSeleccionado.employee_first_name);
+        f.append("employee_second_name", empleadoSeleccionado.employee_second_name);
+        f.append("employee_middle_name", empleadoSeleccionado.employee_middle_name);
+        f.append("employee_last_name", empleadoSeleccionado.employee_last_name);
+        f.append("employee_national_id", empleadoSeleccionado.employee_national_id);
+        f.append("employee_sys_code", empleadoSeleccionado.employee_sys_code);
+        f.append("METHOD", "POST");
+        await axios.post(baseUrl, f)
+        .then(response=>{
+          setData(data.concat(response.data));
+          abrirCerrarModalInsertar();
+        })
+      }
+
+    
+
     useEffect(()=>{
         peticionesGet();
     },[])
 
     const classes = useStyle();
-    const [records, setRecords] = useState(employeeServices.getAllEmployees())
+    //const [records, setRecords] = useState(clientServices.getAllClients())
 
     const {
-        TblContainer,
-        TblHead
-    } = useTable(records.headCells);
+        TblContainer
+    }=useTable();
 
     return(
         <>
@@ -59,7 +106,7 @@ export default function Employee(){
             {/*<EmployeeForm />*/}
             <div style={{textAlign: 'center'}}>
         <   br />
-      <button className="btn btn-success">Insertar Empleado Nuevo</button>
+      <button className="btn btn-success" onClick ={()=>abrirCerrarModalInsertar()}>Insertar Empleado Nuevo</button>
       <br /><br />
     <table className="table table-striped">
       <thead>
@@ -71,7 +118,7 @@ export default function Employee(){
         </tr>
       </thead>
       <tbody>
-            {data.map(employee => (
+            {data.map(employee=> (
                 <tr key={employee.id_employee}>
                     <td>{employee.id_employee}</td>
                     <td>{employee.employee_first_name}</td>
@@ -85,6 +132,68 @@ export default function Employee(){
         </tbody> 
 
     </table>
+    <div className = "modal-dialog modal-lg">
+        <Modal isOpen = {modalInsertar} contentClassName = "custom-modal-style">
+            <ModalHeader>Insertar Empleado</ModalHeader>
+            <ModalBody>
+                {/*<ClientForm/>*/}
+                <div className="form-group">
+                    <Grid container spacing = {2}>
+                        <Grid item xs ={4}>
+                            <label>Primer Nombre: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_first_name" onChange={handleChange}/>
+                            <br />
+                            <label>Segundo Nombre: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_second_name" onChange={handleChange}/>
+                            <br />
+                            <label>Primer Apellido: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_middle_name" onChange={handleChange}/>
+                            <br />
+                            <label>Segundo Apellido: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_last_name" onChange={handleChange}/>
+                            <br />
+                        </Grid>
+                        <Grid item xs ={4}>
+                            <label>Cedula: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_national_id" onChange={handleChange}/>
+                            <br />
+                            <label>Codigo del Sistema: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_sys_code" onChange={handleChange}/>
+                            <br />
+                            <label>Direccion de Casa: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_home_address" onChange={handleChange}/>
+                            <br />
+                            <label>Direccion del Negocio: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_business_address" onChange={handleChange}/>
+                            <br />
+                        </Grid>
+                        <Grid item xs ={4}>
+                            <label>Linea: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_line" onChange={handleChange}/>
+                            <br />
+                            <label>Estado del Cliente: </label>
+                            <br />
+                            <input type="text" className="form-control" name="client_state" onChange={handleChange}/>
+                            <br />   
+                        </Grid>
+                    </Grid>
+                </div> 
+            </ModalBody>
+            <ModalFooter>
+                <button className="btn btn-primary" onClick={()=>peticionesPost()}>Insertar</button>{"   "}
+                <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button>
+            </ModalFooter>
+        </Modal>
+    </div>
 </div>
             
         </Paper>  
