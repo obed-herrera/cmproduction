9c06e8ca9676eeca1fea8d9558ade0525019ae7e
Obed Herrera <skrillexislove@gmail.com>
2021-03-09 15:45:29 -0600

    Cambios en la interfaz de ingreso de clientes

--- src/pages/Clients/Client.js
+++ src/pages/Clients/Client.js
@@ -2,6 +2,24 @@ import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 import axios from 'axios';
+import {Grid, TextField, makeStyles} from '@material-ui/core';
+import  MultipleSelect from "../../controls/MultipleSelect";
+import * as clientServices from '../../services/clientServices';
+import "./ClientStyles.css";
+import {useForm, Form} from '../../components/useForm';
+
+
+const useStyle = makeStyles(theme => ({
+    pageContent:{
+        margin: theme.spacing(5),
+        padding: theme.spacing(3)
+    }
+}))
+
+const clientState = [
+    {id: 'activo', title: 'Activo'},
+    {id: 'inactivo', title: 'Inactivo'},
+]
 
 function Client() {
   const baseUrl="http://localhost/crediapi/client.php";
@@ -13,8 +31,21 @@ function Client() {
     id_credi_client: '',
     client_first_name: '',
     client_second_name: '',
-    client_middle_name: ''
+    client_middle_name: '',
+    client_last_name: '',
+    client_national_id: '',
+    client_sys_code: '',
+    client_home_address: '',
+    client_business_address: '',
+    client_state: '',
+    client_line: '',
+    client_phone: '',
+    client_creation_date: new Date()
   });
+
+  const {
+      values
+  } = useForm(true);
 
   const handleChange=e=>{
     const {name, value}=e.target;
@@ -51,6 +82,14 @@ function Client() {
     f.append("client_first_name", clientSeleccionado.client_first_name);
     f.append("client_second_name", clientSeleccionado.client_second_name);
     f.append("client_middle_name", clientSeleccionado.client_middle_name);
+    f.append("client_last_name", clientSeleccionado.client_last_name);
+    f.append("client_national_id", clientSeleccionado.client_national_id);
+    f.append("client_sys_code", clientSeleccionado.client_sys_code);
+    f.append("client_home_address", clientSeleccionado.client_home_address);
+    f.append("client_business_address", clientSeleccionado.client_business_address);
+    f.append("client_state", clientSeleccionado.client_state);
+    f.append("client_line", clientSeleccionado.client_line);
+    f.append("client_phone", clientSeleccionado.client_phone);
     f.append("METHOD", "POST");
     await axios.post(baseUrl, f)
     .then(response=>{
@@ -118,8 +157,11 @@ function Client() {
         <tr>
           <th>ID</th>
           <th>Primer Nombre</th>
-          <th>Segundo Nombre</th>
           <th>Primer Apellido</th>
+          <th>Codigo del Cliente</th>
+          <th>Direccion de casa</th>
+          <th>Linea</th>
+          <th>Fecha de creación</th>
           <th>Acciones</th>
         </tr>
       </thead>
@@ -128,8 +170,11 @@ function Client() {
           <tr key={index}>
             <td>{client.id_credi_client}</td>
             <td>{client.client_first_name}</td>
-            <td>{client.client_second_name}</td>
             <td>{client.client_middle_name}</td>
+            <td>{client.client_sys_code}</td>
+            <td>{client.client_home_address}</td>
+            <td>{client.client_line}</td>
+            <td>{client.client_creation_date}</td>
           <td>
           <button className="btn btn-primary" onClick={()=>seleccionarClient(client, "Editar")}>Editar</button> {"  "}
           <button className="btn btn-danger" onClick={()=>seleccionarClient(client, "Eliminar")}>Eliminar</button>
@@ -143,23 +188,62 @@ function Client() {
     </table>
 
 
-    <Modal isOpen={modalInsertar}>
-      <ModalHeader>Insertar Framework</ModalHeader>
+    <Modal isOpen={modalInsertar} contentClassName = "custom-modal-style">
+      <ModalHeader>Insertar Cliente</ModalHeader>
       <ModalBody>
-        <div className="form-group">
-          <label>Nombre: </label>
-          <br />
-          <input type="text" className="form-control" name="client_first_name" onChange={handleChange}/>
-          <br />
-          <label>Lanzamiento: </label>
-          <br />
-          <input type="text" className="form-control" name="client_second_name" onChange={handleChange}/>
-          <br />
-          <label>Desarrollador: </label>
-          <br />
-          <input type="text" className="form-control" name="client_middle_name" onChange={handleChange}/>
-          <br />
-        </div>
+            <Grid container spacing = {2} style = {{padding:20}}>
+                <Grid item xs ={4}>
+                    <div className = "form-group">
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "client_first_name" onChange = {handleChange}/>
+                            <span>Primer Nombre</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "client_second_name" onChange = {handleChange}/>
+                            <span>Segundo Nombre</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "client_middle_name" onChange = {handleChange}/>
+                            <span>Primer Apellido</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "client_last_name" onChange = {handleChange}/>
+                            <span>Segundo Apellido</span> 
+                        </label>
+                        <br/>
+                        {/*<input placeholder= " " type = "text" className = "form-control" name = "client_first_name" onChange = {handleChange}/>*/}
+                    </div>
+                </Grid>
+                <Grid item xs ={4}> 
+                    <div className = "form-group">
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "client_national_id" onChange = {handleChange}/>
+                            <span>Cedula del Cliente</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "client_sys_code" onChange = {handleChange}/>
+                            <span>Codigo del Cliente</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "client_home_address" onChange = {handleChange}/>
+                            <span>Direccion de casa</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "client_business_address" onChange = {handleChange}/>
+                            <span>Direccion de negocio</span> 
+                        </label>
+                        <br/>
+                    </div>                                    
+                </Grid>
+                <Grid item xs = {4}>
+                </Grid>
+            </Grid>
       </ModalBody>
       <ModalFooter>
         <button className="btn btn-primary" onClick={()=>peticionPost()}>Insertar</button>{"   "}
--- src/services/clientServices.js
+++ src/services/clientServices.js
@@ -30,3 +30,19 @@ export function getAllClients(){
         localStorage.setItem(KEYS.client, JSON.stringify([]))
     return JSON.parse(localStorage.getItem(KEYS.client));
 }
+
+export const getLines = () => ([
+    {id: '1', title: 'Huembes'},
+    {id: '2', title: 'Oriental'},
+    {id: '3', title: 'Montetabor'},
+    {id: '4', title: 'TICOMO'},
+    {id: '5', title: 'San Jose O.'},
+    {id: '6', title: 'Cuajachillo'},
+    {id: '7', title: 'Ciudad Sandino'},
+    {id: '8', title: 'Villa Reconciliacion'},
+    {id: '9', title: 'Bello Amanecer 1'},
+    {id: '10', title: 'Bello Amanecer 2'},
+    {id: '11', title: 'Bello Amanecer 3'},
+    {id: '12', title: 'Bello Amanecer 4'},
+    {id: '13', title: 'Los Brasiles'},
+]);
