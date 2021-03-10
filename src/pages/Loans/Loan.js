d6657ff59a25fe844c84469fc222076092c90fec
Obed Herrera <skrillexislove@gmail.com>
2021-03-09 20:43:15 -0600

    Inicio de la parte de los loans

--- src/pages/Loans/Loan.js
+++ src/pages/Loans/Loan.js
@@ -1,9 +1,14 @@
-import {React, useState} from 'react'
-import LoanForm from "./LoanForm";
-import PageHeader from "../../components/PageHeader";
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney'
-import {Paper, makeStyles, TableBody, TableRow, TableCell} from '@material-ui/core';
-import Copyright from '../../components/Copyright';
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import axios from 'axios';
+import {Grid, TextField, makeStyles} from '@material-ui/core';
+import  MultipleSelect from "../../controls/MultipleSelect";
+import * as loanServices from '../../services/loanServices';
+import "./LoanStyles.css";
+import {useForm, Form} from '../../components/useForm';
+import SideMenu from "../../components/SideMenu";
+
 
 const useStyle = makeStyles(theme => ({
     pageContent:{
@@ -12,21 +17,210 @@ const useStyle = makeStyles(theme => ({
     }
 }))
 
-export default function Loan(){
+const loanState = [
+    {id: 'activo', title: 'Activo'},
+    {id: 'inactivo', title: 'Inactivo'},
+]
 
-    const classes = useStyle();
+function Loan() {
+  const baseUrl="http://localhost/crediapi/Loan.php";
+  const [data, setData]=useState([]);
+  const [modalInsertar, setModalInsertar]= useState(false);
+  const [modalEditar, setModalEditar]= useState(false);
+  const [modalEliminar, setModalEliminar]= useState(false);
+  const [loanSeleccionado, setloanSeleccionado]=useState({
+    id_credi_loan: '',
+    loan_client: '',
+  });
 
-    return(
-        <>
-        <PageHeader 
-            title ="Préstamo nuevo"
-            subTitle = "Formulario para crear un Préstamo Nuevo"
-            icon = {<AttachMoneyIcon fontSize = "large" />}
-        /> 
-        <Paper className={classes.pageContent}>
-            {<LoanForm />}           
-        </Paper>
-        <Copyright/>  
-        </>
-    )
+
+  const handleChange=e=>{
+    const {name, value}=e.target;
+    setloanSeleccionado((prevState)=>({
+      ...prevState,
+      [name]: value
+    }))
+    console.log(loanSeleccionado);
+  }
+
+  const abrirCerrarModalInsertar=()=>{
+    setModalInsertar(!modalInsertar);
+  }
+
+  const abrirCerrarModalEditar=()=>{
+    setModalEditar(!modalEditar);
+  }
+
+  const abrirCerrarModalEliminar=()=>{
+    setModalEliminar(!modalEliminar);
+  }
+
+  const peticionGet=async()=>{
+    await axios.get(baseUrl)
+    .then(response=>{
+      setData(response.data);
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  const peticionPost=async()=>{
+    var f = new FormData();
+    f.append("loan_first_name", loanSeleccionado.loan_client);
+    f.append("METHOD", "POST");
+    await axios.post(baseUrl, f)
+    .then(response=>{
+      setData(data.concat(response.data));
+      abrirCerrarModalInsertar();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  /*const peticionPut=async()=>{
+    var f = new FormData();
+    f.append("nombre", loanSeleccionado.nombre);
+    f.append("lanzamiento", loanSeleccionado.lanzamiento);
+    f.append("desarrollador", loanSeleccionado.desarrollador);
+    f.append("METHOD", "PUT");
+    await axios.post(baseUrl, f, {params: {id: frameworkSeleccionado.id}})
+    .then(response=>{
+      var dataNueva= data;
+      dataNueva.map(framework=>{
+        if(framework.id===frameworkSeleccionado.id){
+          framework.nombre=frameworkSeleccionado.nombre;
+          framework.lanzamiento=frameworkSeleccionado.lanzamiento;
+          framework.desarrollador=frameworkSeleccionado.desarrollador;
+        }
+      });
+      setData(dataNueva);
+      abrirCerrarModalEditar();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }*/
+
+  /*const peticionDelete=async()=>{
+    var f = new FormData();
+    f.append("METHOD", "DELETE");
+    await axios.post(baseUrl, f, {params: {id: frameworkSeleccionado.id}})
+    .then(response=>{
+      setData(data.filter(framework=>framework.id!==frameworkSeleccionado.id));
+      abrirCerrarModalEliminar();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }*/
+
+  const seleccionarloan=(loan, caso)=>{
+    setloanSeleccionado(loan);
+
+    (caso==="Editar")?
+    abrirCerrarModalEditar():
+    abrirCerrarModalEliminar()
+  }
+
+  useEffect(()=>{
+    peticionGet();
+  },[])
+
+  return (
+    <div style={{textAlign: 'center'}}>
+<br />
+      <button className="btn btn-success" onClick={()=>abrirCerrarModalInsertar()}>Insertar</button>
+      <br /><br />
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>ID Prestamo</th>
+          <th>Cliente</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data && data.map((loan, index)=>(
+          <tr key={index}>
+            <td>{loan.id_credi_loan}</td>
+            <td>{loan.loan_client}</td>
+          <td>
+          <button className="btn btn-primary" onClick={()=>seleccionarloan(loan, "Editar")}>Editar</button> {"  "}
+          <button className="btn btn-danger" onClick={()=>seleccionarloan(loan, "Eliminar")}>Eliminar</button>
+          </td>
+          </tr>
+        ))}
+
+
+      </tbody> 
+
+    </table>
+
+
+    <Modal isOpen={modalInsertar} contentClassName = "custom-modal-style">
+      <ModalHeader>Insertar loane</ModalHeader>
+      <ModalBody>
+            <Grid container spacing = {2} style = {{padding:20}}>
+                <Grid item xs ={4}>
+                    <div className = "form-group">
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "loan_client" onChange = {handleChange}/>
+                            <span>Cliente</span>
+                        </label>
+                        {/*<input placeholder= " " type = "text" className = "form-control" name = "loan_first_name" onChange = {handleChange}/>*/}
+                    </div>
+                </Grid>
+                <Grid item xs ={4}> 
+                    <div className = "form-group">
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "loan_national_id" onChange = {handleChange}/>
+                            <span>Cedula del loane</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "loan_sys_code" onChange = {handleChange}/>
+                            <span>Codigo del loane</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "loan_home_address" onChange = {handleChange}/>
+                            <span>Direccion de casa</span> 
+                        </label>
+                        <br/>
+                        <label class = "pure-material-textfield-outlined">
+                            <input placeholder= " " type = "text" className = "form-control" name = "loan_business_address" onChange = {handleChange}/>
+                            <span>Direccion de negocio</span> 
+                        </label>
+                        <br/>
+                    </div>                                    
+                </Grid>
+                <Grid item xs = {4}>
+                  <div className = "form-group">
+                  <label class = "pure-material-textfield-outlined">
+                      <input placeholder= " " type = "text" className = "form-control" name = "loan_phone" onChange = {handleChange}/>
+                      <span>Telefono del loane</span> 
+                    </label>
+                    <br/>
+                    <label class = "pure-material-textfield-outlined">
+                      <input placeholder= " " type = "text" className = "form-control" name = "loan_state" onChange = {handleChange}/>
+                      <span>Estado del loane</span> 
+                    </label>
+                    <br/>
+                    <label class = "pure-material-textfield-outlined">
+                      <input placeholder= " " type = "text" className = "form-control" name = "loan_line" onChange = {handleChange}/>
+                      <span>Linea del loane</span> 
+                    </label>
+                    <br/>
+                  </div>
+                </Grid>
+            </Grid>
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn btn-primary" onClick={()=>peticionPost()}>Insertar</button>{"   "}
+        <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button>
+      </ModalFooter>
+    </Modal>
+
+    </div>
+  );
 }
+
+export default Loan;
