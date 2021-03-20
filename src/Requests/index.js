04074ef3494bcb628054ea45dc72f612b01c37b3
Obed Herrera <skrillexislove@gmail.com>
2021-03-19 20:57:29 -0600

    Metodo para mandar a llamar las lineas

--- src/Requests/index.js
+++ src/Requests/index.js
@@ -4,9 +4,24 @@ const url = axios.create({
     baseURL: 'http://localhost/crediapi/client.php',
 });
 
+const url2 = axios.create({
+    baseURL: 'http://localhost/crediapi/line.php',
+});
+
 export const getClients = (body) =>{
     let result = url
     .get('/client')
+    .then((response)=>{
+        return response.data;
+    }).catch((error)=>{
+        return error;
+    });
+    return result;
+}
+
+export const getLines = (body) =>{
+    let result = url2
+    .get('/line')
     .then((response)=>{
         return response.data;
     }).catch((error)=>{
