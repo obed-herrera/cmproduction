fce060be2d19d21d274bd98d36acf60786f1b325
Obed Herrera <skrillexislove@gmail.com>
2021-03-26 22:56:31 -0600

    Ahora home posee el metodo route de react-router y el sidemenu funciona bien

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import Typography from '@material-ui/core/Typography'
+import SideMenu from '../../components/SideMenu';
+import {Switch, Route} from 'react-router-dom';
+import Client from '../Clients/Client';
+import Employee from "../Employees/Employee";
+import Configuration from "../Configuration/Configuration";
+import CreateUser from "../Configuration/CreateUser";
+import CreateLine from "../Configuration/CreateLine";
+import Loan from '../Loans/Loan';
+import {NavLink, Link} from 'react-router-dom';
 
-export default function Home(){
+const Home = () =>{
     return(
         <div>
-            <Typography align = 'center'>Hola que tal bienvenido a CrediMarket</Typography>
-            
+            <SideMenu/>
+            <Switch>
+                <Route path = '/client' component = {Client}/>
+                <Route path = '/employee' component = {Employee}/>
+                <Route path = '/loan' component = {Loan}/>
+                <Route path = '/configuration' component = {Configuration}/>
+                <Route path = '/createuser' component = {CreateUser}/>  
+                <Route path = '/createline' component = {CreateLine}/>
+            </Switch>
         </div>
-
     )
 }
+
+export default Home;
