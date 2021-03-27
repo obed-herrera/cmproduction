9b2a5e82b83749f0da0172c99598f5c9babdd7cc
Obed Herrera <skrillexislove@gmail.com>
2021-03-26 22:55:41 -0600

    Cambios menores

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
-import Login from './pages/Login/Login';
 import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
  
